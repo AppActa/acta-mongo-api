dd76d08ffca5925c939b639ba82f22fb108f9e9d
Davi Aliaga <kavhsao@gmail.com>
2026-08-20 22:52:54 -0300

    feat/projeto: adiciona estrutura inicial com configuração de maven e spring boot

---
+++ src/main/java/br/com/picpay/actamongoapi/ActaMongoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.picpay.actamongoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActaMongoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActaMongoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/picpay/actamongoapi/ActaMongoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.picpay.actamongoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActaMongoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
